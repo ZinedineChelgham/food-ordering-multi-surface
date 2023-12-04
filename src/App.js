463b5d2bdc7d24f36cf2f9efdd79ebed134e4bcb
ELYSALEM99 <elysalemm5@gmail.com>
2023-12-04 09:40:44 +0100

    Add Componets OrderHeader(Total , prix, ...) and  CommandList

--- src/App.js
+++ src/App.js
@@ -3,23 +3,41 @@ import FoodItemsList from "./components/FoodItemList";
 import IconWithLabelCard from "./components/common/IconWithLabelCard";
 import NavBar from "./components/NavBar";
 import Grid2 from "@mui/material/Unstable_Grid2";
+import OrderHeader from './components/OrderHeader';
+import CommandList from './components/CommandList';
+
 
 
 function App() {
 
     return (
-        <Grid2 container
-            direction="row"
-            width={'100%'}
-            height={'100%'}
-            xs={12}
-            margin={0}
-            padding={0}
-            boxSizing={'border-box'}
-            backgroundColor={'violet'}
-        >
-            <NavBar />
-            <FoodItemsList />
+        <Grid2 container direction="column" width={'100%'} height={'100%'} xs={12} margin={0} padding={0} boxSizing={'border-box'} backgroundColor={'violet'}>
+            <Grid2 container
+                direction="row"
+                width={'100%'}
+                height={'100%'}
+                xs={12}
+                margin={0}
+                padding={0}
+                boxSizing={'border-box'}
+                backgroundColor={'violet'}
+            >
+                <NavBar />
+                <FoodItemsList />
+            </Grid2>
+            <Grid2 container
+                direction="column"
+                width={'100%'}
+                height={'100%'}
+                xs={12}
+                margin={0}
+                padding={0}
+                boxSizing={'border-box'}
+                backgroundColor={'violet'}>
+                <OrderHeader />
+                <CommandList />
+            </Grid2>
+            
         </Grid2>
     );
 }
---
+++ src/components/CommandList.js
@@ -0,0 +1,12 @@
+// EmptyOrderMessage.js
+import React from 'react';
+
+const CommandList = () => {
+  return (
+    <div style={{ backgroundColor: 'white',border: '2px solid black', padding: '20px', textAlign: 'center' }}>
+      <p>VOTRE COMMANDE EST VIDE</p>
+    </div>
+  );
+};
+
+export default CommandList;
