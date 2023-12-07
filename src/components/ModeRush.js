02c04edee07c90d94434699d4fa9a7529bd62fc8
ELYSALEM99 <elysalemm5@gmail.com>
2023-12-07 22:20:36 +0100

    create dossier rush pour stockes les images

--- src/components/ModeRush.js
+++ src/components/ModeRush.js
@@ -2,18 +2,18 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 import ProductCard from "./common/ProductCard";
-import image1 from '../assets/img/burger/image_1.png';
-import image2 from '../assets/img/burger/image_2.png';
-import image3 from "../assets/img/burger/image_3.png";
-import image4 from "../assets/img/burger/image_4.png";
-import image5 from "../assets/img/burger/image_5.png";
-import image6 from "../assets/img/burger/image_6.png";
-import image7 from "../assets/img/burger/image_7.png";
-import image8 from "../assets/img/burger/image_8.png";
-import image9 from "../assets/img/burger/image_9.png";
-import image10 from "../assets/img/burger/image_10.png";
-import image11 from "../assets/img/burger/image_11.png";
-import image12 from "../assets/img/burger/image_12.png";
+import image1 from '../assets/img/rush/image_1.png';
+import image2 from '../assets/img/rush/image_2.png';
+import image3 from "../assets/img/rush/image_3.png";
+import image4 from "../assets/img/rush/image_4.png";
+import image5 from "../assets/img/rush/image_5.png";
+import image6 from "../assets/img/rush/image_6.png";
+import image7 from "../assets/img/rush/image_7.png";
+import image8 from "../assets/img/rush/image_8.png";
+import image9 from "../assets/img/rush/image_9.png";
+import image10 from "../assets/img/rush/image_10.png";
+import image11 from "../assets/img/rush/image_11.png";
+import image12 from "../assets/img/rush/image_12.png";
 
 const images = [
     image1,
