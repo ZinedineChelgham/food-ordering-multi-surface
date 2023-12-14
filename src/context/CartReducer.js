625e94066c49a816e80d0db70f4694a7fd9d5ad2
ZC <zinedine.chelgham@etu.univ-cotedazur.fr>
2023-12-14 23:58:20 +0100

    order container of borne fully linked to cart

--- src/context/CartReducer.js
+++ src/context/CartReducer.js
@@ -17,9 +17,7 @@ const CartReducer = (state, action) => {
     case REMOVE_ITEM: {
       return {
         ...state,
-        cartItems: state.cartItems.filter(
-          (item) => item.name !== action.payload
-        ),
+        cartItems: state.cartItems.filter((item) => item.id !== action.payload),
       };
     }
 
