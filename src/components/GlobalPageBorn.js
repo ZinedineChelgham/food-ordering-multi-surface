aaa0fbbd01a8a13eb5575ad949094e5fc9a83a80
ZC <zinedine.chelgham@etu.univ-cotedazur.fr>
2023-12-16 21:30:22 +0100

    Refactor BackButtonHeader component and add Typography component

--- src/components/GlobalPageBorn.js
+++ src/components/GlobalPageBorn.js
@@ -12,33 +12,62 @@ import foodItems from "../data";
 import ModeRush from "./ModeRush";
 import IconButton from "@mui/material/IconButton";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import Typography from "@mui/material/Typography";
 
 function BackButtonHeader() {
-    // Vous pouvez ajouter une fonction pour gérer l'événement onClick si nécessaire.
-    const handleBack = () => {
-        // Par exemple, naviguer vers la page précédente.
-        console.log('Retour');
-    };
+  // Vous pouvez ajouter une fonction pour gérer l'événement onClick si nécessaire.
+  const handleBack = () => {
+    // Par exemple, naviguer vers la page précédente.
+    console.log("Retour");
+  };
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isLandscape = useMediaQuery("(orientation: landscape)");
 
-    return (
-        <div style={{ padding: '1vh' }}>
-            <IconButton onClick={handleBack}
-                        style={{fontSize:'5vh'}}>
-                <ArrowBackIcon style={{ fontSize: 'inherit' }} />
-            </IconButton>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "1vh",
+      }}
+    >
+      {/* Vous pouvez toujours garder l'IconButton si vous souhaitez avoir un bouton de retour */}
+      <IconButton
+        onClick={handleBack}
+        style={{ position: "absolute", left: 0, fontSize: "5vh" }}
+      >
+        <ArrowBackIcon style={{ fontSize: "inherit" }} />
+      </IconButton>
+      {/* Titre de l'application */}
+      <Typography
+        variant="h4"
+        component="h1"
+        style={{
+          flexGrow: 1,
+          textAlign: "center",
+          letterSpacing: "0.1em",
+          textShadow: "2px 2px 4px rgba(0,0,0,0.3)",
+          fontSize: isSmallScreen ? "5vw" : isLandscape ? "5vh" : "6vw",
+          fontWeight: "bold",
+        }}
+      >
+        Borne'n'Go
+      </Typography>
+    </div>
+  );
 }
 
 function GlobalPageBorne(props) {
-    const theme = useTheme();
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
-    const isLandscape = useMediaQuery("(orientation: landscape)");
-    const [isRushMode, setIsRushMode] = React.useState(false);
-    const ingredients = ["tomate", "salade", "fromage"];
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isLandscape = useMediaQuery("(orientation: landscape)");
+  const [isRushMode, setIsRushMode] = React.useState(false);
+  const ingredients = ["tomate", "salade", "fromage"];
 
-    React.useEffect(() => {
-        /*const interval = setInterval(() => {
+  React.useEffect(() => {
+    /*const interval = setInterval(() => {
             fetch('http://localhost:3001/mode-rush')
                 .then(response => {
                     if (!response.ok) {
@@ -53,48 +82,46 @@ function GlobalPageBorne(props) {
         }, 10000); // Vérifie toutes les 10 secondes
         return () => clearInterval(interval);
          */
-    }, []);
+  }, []);
 
+  const [category, setCategory] = React.useState("menus");
 
-    const [category, setCategory] = React.useState("menus");
+  console.log("item from borne", foodItems[category]);
 
-    console.log("item from borne", foodItems[category]);
+  const handleCategoryChange = (cat) => {
+    setCategory(cat.toLowerCase());
+    console.log("clicked" + cat);
+  };
 
-    const handleCategoryChange = (cat) => {
-        setCategory(cat.toLowerCase());
-        console.log("clicked" + cat);
-    };
+  console.log("-----borne display-----");
 
-    console.log("-----borne display-----");
-
-
-    return (
-        <div style={{ height: "100vh", overflow: "hidden" }}>
-            <div style={{ height: "auto", overflow: "hidden" }}>
-                <BackButtonHeader/>
-            </div>
-            <div style={{ height: "65%", overflow: "hidden" }}>
-                {/* Le composant OrderBar prendra tout l'espace disponible */}
-                {/* <NavPlusList
+  return (
+    <div style={{ height: "100vh", overflow: "hidden" }}>
+      <div style={{ height: "auto", overflow: "hidden" }}>
+        <BackButtonHeader />
+      </div>
+      <div style={{ height: "65%", overflow: "hidden" }}>
+        {/* Le composant OrderBar prendra tout l'espace disponible */}
+        {/* <NavPlusList
                     handleCategoryChange={handleCategoryChange}
                     foodItems={foodItems[category]}
                     currCat={category}
                 /> */}
-                {isRushMode ? (
-                    <ModeRush ingredients={ingredients}/>
-                ) : (
-                    <NavPlusList
-                        handleCategoryChange={handleCategoryChange}
-                        foodItems={foodItems[category]}
-                        currCat={category}
-                        />
-                )}
-            </div>
-            <div style={{ height: "25%" }}>
-                <OrderContainer />
-            </div>
-        </div>
-    );
+        {isRushMode ? (
+          <ModeRush ingredients={ingredients} />
+        ) : (
+          <NavPlusList
+            handleCategoryChange={handleCategoryChange}
+            foodItems={foodItems[category]}
+            currCat={category}
+          />
+        )}
+      </div>
+      <div style={{ height: "25%" }}>
+        <OrderContainer />
+      </div>
+    </div>
+  );
 }
 
 export default GlobalPageBorne;
